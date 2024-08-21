14d18032023941addb113d7caf12c450528bb396
cindyweng18 <cindyweng18@gmail.com>
2024-08-21 04:55:20 -0400

    successfully implemented rag

--- rmp-ai-assistant-python/setup_rag.py
+++ rmp-ai-assistant-python/setup_rag.py
@@ -1,4 +1,3 @@
-# TODO: Change code to use Gemini API
 from dotenv import load_dotenv
 load_dotenv()
 from pinecone import Pinecone, ServerlessSpec
@@ -16,7 +15,7 @@ pc = Pinecone(api_key=os.getenv("PINECONE_API_KEY"))
 # Create a Pinecone index
 pc.create_index(
     name="rag",
-    dimension=1536,
+    dimension=768,
     metric="cosine",
     spec=ServerlessSpec(cloud="aws", region="us-east-1"),
 )
@@ -37,7 +36,7 @@ for review in data["reviews"]:
     # response = client.embeddings.create(
     #     input=review['review'], model="text-embedding-3-small"
     # )
-    embedding = str(response['embedding'])[:50]
+    embedding = response['embedding']
 
     # embedding = response.data[0].embedding
     processed_data.append(
@@ -53,6 +52,7 @@ for review in data["reviews"]:
     )
 
 # Insert the embeddings into the Pinecone index
+
 index = pc.Index("rag")
 upsert_response = index.upsert(
     vectors=processed_data,
