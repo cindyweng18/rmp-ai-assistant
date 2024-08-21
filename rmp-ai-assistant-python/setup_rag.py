2f9ed024abd8af41f7a85de20d5cfc7698eda902
cindyweng18 <cindyweng18@gmail.com>
2024-08-21 04:26:09 -0400

    installed gemini embeddings ai

--- rmp-ai-assistant-python/setup_rag.py
+++ rmp-ai-assistant-python/setup_rag.py
@@ -2,10 +2,14 @@
 from dotenv import load_dotenv
 load_dotenv()
 from pinecone import Pinecone, ServerlessSpec
-from openai import OpenAI
+# from openai import OpenAI
+import google.generativeai as genai
 import os
 import json
 
+# Find env local file
+env_path = os.path.join(os.path.dirname(__file__), '../.env.local')
+load_dotenv(dotenv_path=env_path)
 # Initialize Pinecone
 pc = Pinecone(api_key=os.getenv("PINECONE_API_KEY"))
 
@@ -21,14 +25,21 @@ pc.create_index(
 data = json.load(open("reviews.json"))
 
 processed_data = []
-client = OpenAI()
+# model = genai.GenerativeModel('gemini-1.5-flash')
+# client = OpenAI()
+genai.configure(api_key=os.getenv("GEMINI_API_KEY"))
 
 # Create embeddings for each review
 for review in data["reviews"]:
-    response = client.embeddings.create(
-        input=review['review'], model="text-embedding-3-small"
-    )
-    embedding = response.data[0].embedding
+    response = genai.embed_content(
+        content=review['review'], 
+        model="models/text-embedding-004")
+    # response = client.embeddings.create(
+    #     input=review['review'], model="text-embedding-3-small"
+    # )
+    embedding = str(response['embedding'])[:50]
+
+    # embedding = response.data[0].embedding
     processed_data.append(
         {
             "values": embedding,
